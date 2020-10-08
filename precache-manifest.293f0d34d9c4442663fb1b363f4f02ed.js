e7c8be9c62c46a101ac0ae1fae04f6dfad4addb3
EzequielCaste <ezequiel.caste@gmail.com>
2020-10-08 10:37:02 -0300

    Updates

---
+++ precache-manifest.293f0d34d9c4442663fb1b363f4f02ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020e2a8cfa343568aba760a0d4017001",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f50936e7ca78ce99dd3b",
+    "url": "/weather-app/static/css/main.6f342386.chunk.css"
+  },
+  {
+    "revision": "622a3674b7aac1612425",
+    "url": "/weather-app/static/js/2.198be48b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.198be48b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50936e7ca78ce99dd3b",
+    "url": "/weather-app/static/js/main.5f37b1df.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
