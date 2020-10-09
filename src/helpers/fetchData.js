f0ebece355c3c2b3de944d83f35f7156adb03309
EzequielCaste <ezequiel.caste@gmail.com>
2020-10-08 21:35:44 -0300

    Update fetchData.js

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -4,7 +4,7 @@ export const fetchData = async( {latitude: lat, longitude: lon} ) => {
   //const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=24d7eab231aa80194d6732c8effdded4`
 
   // Daily forecast for 7 days
-  const url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&appid=${process.env.REACT_APP_API_KEY}&units=metric`
 
   const resp = await fetch(url);
   const data = await resp.json();
